eb5efa85c0784ea967fdaed3a07f2aeb900f2e92
Evan <41494790+evaan@users.noreply.github.com>
2024-02-03 00:39:29 -0330

    Finished (for now) index and started projects

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
 			fontFamily: {
-				"inter": ["Inter", "monospace"]
-			}
+				"inter": ["Inter"],
+			},
 		},
 	},
 	plugins: [],
