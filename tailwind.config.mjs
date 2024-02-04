4f07c0da9dff63d02d429883df9947fbca4e4ce2
Evan <41494790+evaan@users.noreply.github.com>
2024-02-04 00:47:20 -0330

    Misc Tweaks

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 
+const plugin = require('tailwindcss/plugin')
+
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
@@ -11,6 +13,9 @@ export default {
 	},
 	plugins: [
 		require('@tailwindcss/typography'),
+		plugin(function ({ addVariant }) {
+			addVariant('mobile-only', "@media screen and (max-width: theme('screens.sm'))");
+		}),
 	],
 	site: 'https://evaan.dev',
 }
